9d2e1d9ad947af8d0d8fe8f45b3e779070ead5ad
황대찬 <ghkdeocks123@naver.com>
2025-04-01 00:12:04 +0900

    20250401 배포를 위한 수정중10

---
+++ lib/config/naverapi_config.dart
@@ -0,0 +1,6 @@
+// lib/config/naverapi_config.dart
+class NaverApiConfig {
+  static const String baseUrl = '1';
+  static const String clientId = '1'; // 네이버 Client ID
+  static const String clientSecret = '1'; // 네이버 Client Secret
+}
